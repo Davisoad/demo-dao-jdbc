d62711386ad7f7c9b08de0631c1eb3ccda657946
Davysk <davibragaz18@gmail.com>
2022-12-15 10:14:44 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,8 @@ import model_dao.SellerDao;
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.crateSellerDao();
-        
+        System.out.println("=== TEST 1: seller findById =====");
         Seller seller = sellerDao.finById(3);
-
         System.out.println(seller);
     }
 }
